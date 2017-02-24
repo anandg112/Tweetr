d172432c200019abbb6cfa6bd7909a3f172eccbb
Anand <anandg112@gmail.com>
2017-02-24 16:18:50 +0000

    made changes to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,33 @@
 //  */
 //
 
+function timeDifference(current, previous) {
+  let msPerMinute = 60 * 1000;
+  let msPerHour = msPerMinute * 60;
+  let msPerDay = msPerHour * 24;
+  let msPerMonth = msPerDay * 30;
+  let msPerYear = msPerDay * 365;
+  let elapsed = current - previous;
+  if (elapsed < msPerMinute) {
+      return Math.round(elapsed/1000) + ' seconds ago';
+  }
+  else if (elapsed < msPerHour) {
+      return Math.round(elapsed/msPerMinute) + ' minutes ago';
+  }
+  else if (elapsed < msPerDay ) {
+      return Math.round(elapsed/msPerHour ) + ' hours ago';
+  }
+  else if (elapsed < msPerMonth) {
+     return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';
+  }
+  else if (elapsed < msPerYear) {
+     return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';
+  }
+  else {
+     return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';
+  }
+}
+
 function renderTweets(tweets) {
    // loops through tweets
      // calls createTweetElement for each tweet
@@ -23,8 +50,8 @@ function renderTweets(tweets) {
  }
 
  function createTweetElement(tweet){
-   var output = "";
-
+   let output = "";
+   let td = timeDifference(Date.now(), tweet.created_at);
    output = `<article class="user-tweet">
              <header>
                 <img class="avatar"  src=${tweet.user.avatars.small}>
@@ -34,7 +61,7 @@ function renderTweets(tweets) {
               <p class="tweet-text">${escape(tweet.content.text)}</p>
 
              <footer class="footer">
-               <p>${(new Date (tweet.created_at)).toDateString()}</p>
+               <p>${td}</p>
                <div class="icons">
                  <i class="material-icons">favorite</i>
                  <i class="material-icons">cached</i>
